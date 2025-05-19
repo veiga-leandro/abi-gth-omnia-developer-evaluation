4630068d63b7b5d31eb1a308b8e8e8cb58042a4c
Leandro Camargo <13659697+veiga-leandro@users.noreply.github.com>
2025-05-18 22:02:07 -0400

    adjusted swagger authorization

--- template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Program.cs
@@ -5,6 +5,7 @@ using Ambev.DeveloperEvaluation.Common.Security;
 using Ambev.DeveloperEvaluation.Common.Validation;
 using Ambev.DeveloperEvaluation.IoC;
 using Ambev.DeveloperEvaluation.ORM;
+using Ambev.DeveloperEvaluation.WebApi.Filters;
 using Ambev.DeveloperEvaluation.WebApi.Middleware;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -48,23 +49,7 @@ public class Program
                     Scheme = "Bearer"
                 });
 
-                c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                {
-                    {
-                        new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            },
-                            Scheme = "oauth2",
-                            Name = "Bearer",
-                            In = ParameterLocation.Header
-                        },
-                        new List<string>()
-                    }
-                });
+                c.OperationFilter<AuthorizeCheckOperationFilter>();
 
                 // Configure Swagger to use the XML comments file
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
